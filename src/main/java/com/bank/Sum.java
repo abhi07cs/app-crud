476a5b606a71ef4e6dad7fd71ceeac9a129b8788
Abhishek kumar <abhi7cs@gmail.com>
2026-02-01 23:21:42 +0530

    second commit

--- src/main/java/com/bank/Sum.java
+++ src/main/java/com/bank/Sum.java
@@ -5,7 +5,7 @@ public class Sum {
     public static void main(String[] args){
         int a=10;
         int b=20;
-        System.out.println(a+b);
+        System.out.println("Sum of " +( a+b));
 
     }
 }
